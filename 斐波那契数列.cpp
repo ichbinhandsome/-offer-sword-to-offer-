f5f022eb5de54ea75bd0720ce10cc6742fa227ad
ruixiang <wangruixiang07@outlook.com>
2020-06-01 18:10:07 +0200

    2020.6.1

---
+++ 斐波那契数列.cpp
@@ -0,0 +1,28 @@
+/*
+写一个函数，输入 n ，求斐波那契（Fibonacci）数列的第 n 项。斐波那契数列的定义如下：
+
+F(0) = 0,   F(1) = 1
+F(N) = F(N - 1) + F(N - 2), 其中 N > 1.
+斐波那契数列由 0 和 1 开始，之后的斐波那契数就是由之前的两数相加而得出。
+
+答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/fei-bo-na-qi-shu-lie-lcof
+*/
+class Solution {
+public:
+    int fib(int n) {
+        if (n<=1){
+            return n;
+        }
+        int a = 0, b = 1;
+        for(int i = 0; i < n-1; i++){
+            int temp = a;
+            a = b % 1000000007;
+            b = (b+ temp)% 1000000007;
+        }
+        return b;
+    }
+
+};
