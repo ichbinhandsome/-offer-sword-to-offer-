97b29b5ed8ea756041086d0fc64e5c99ae26895d
ruixiang <wangruixiang07@outlook.com>
2020-06-11 15:55:08 +0200

    2020.6.11

---
+++ 链表/从尾到头打印链表.cpp
@@ -0,0 +1,35 @@
+/*
+输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+
+ 
+
+示例 1：
+
+输入：head = [1,3,2]
+输出：[2,3,1]
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> reversePrint(ListNode* head) {
+        //辅助栈
+        stack<int> s;
+        vector<int> res;
+        while (head){
+            s.push(head->val);
+            head = head->next;
+        }
+        while (!s.empty()){
+            res.push_back(s.top());
+            s.pop();
+        }
+        return res;
+    }
+};
---
+++ 链表/从尾到头打印链表.py
@@ -0,0 +1,30 @@
+'''
+输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+
+ 
+
+示例 1：
+
+输入：head = [1,3,2]
+输出：[2,3,1]
+'''
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reversePrint(self, head: ListNode) -> List[int]:
+        if not head:
+            return []
+        #递归
+        self.res = []
+        def helper(node):
+            if not node:
+                return
+            helper(node.next)
+            #回溯
+            self.res.append(node.val)
+        helper(head)
+        return self.res
