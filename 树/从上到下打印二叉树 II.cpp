700b8503f5b04b34f6515e08d7b03096a4880db4
ruixiang <wangruixiang07@outlook.com>
2020-06-18 23:24:38 +0200

    2020.6.18

---
+++ 树/从上到下打印二叉树 II.cpp
@@ -0,0 +1,56 @@
+/*
+从上到下按层打印二叉树，同一层的节点按从左到右的顺序打印，每一层打印到一行。
+
+ 
+
+例如:
+给定二叉树: [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回其层次遍历结果：
+
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-ii-lcof
+*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        if (!root) return res;
+        queue<TreeNode*> q;
+        q.push(root);
+        while (!q.empty()){
+            int n = q.size();
+            vector<int> temp;
+            for(int i=0; i<n; i++){
+                TreeNode* node = q.front();
+                q.pop();
+                temp.push_back(node->val);
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+            // cout<temp<endl;
+            res.push_back(temp);
+        }
+        return res;
+    }
+};
