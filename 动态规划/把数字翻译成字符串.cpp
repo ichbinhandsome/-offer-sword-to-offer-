656f788e1c3654636ea09553510a498f1ca9f211
ruixiang <wangruixiang07@outlook.com>
2020-08-10 22:06:16 +0200

    2020.8.10

---
+++ 动态规划/把数字翻译成字符串.cpp
@@ -0,0 +1,23 @@
+/*
+给定一个数字，我们按照如下规则把它翻译为字符串：0 翻译成 “a” ，1 翻译成 “b”，……，11 翻译成 “l”，……，25 翻译成 “z”。一个数字可能有多个翻译。请编程实现一个函数，用来计算一个数字有多少种不同的翻译方法。
+
+ 
+
+示例 1:
+
+输入: 12258
+输出: 5
+解释: 12258有5种不同的翻译，分别是"bccfi", "bwfi", "bczi", "mcfi"和"mzi"
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/ba-shu-zi-fan-yi-cheng-zi-fu-chuan-lcof
+*/
+class Solution {
+public:
+    int translateNum(int num) {
+        // 递归求解
+        if (num < 10) return 1;
+        //取后两位数判断
+        return (num%100 < 10 || num%100 > 25) ? translateNum(num/10) : translateNum(num/10) + translateNum(num/100);
+    }
+};
